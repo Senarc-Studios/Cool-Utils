32d95d09d16582e91f2a1e9dca7144d3475069dc
Benitz Original <benitz@numix.xyz>
2021-10-23 19:45:55 +0800

    Updated Versions

--- utils/__init__.py
+++ utils/__init__.py
@@ -34,8 +34,9 @@ __title__ = 'cool-utils'
 __author__ = 'BenitzCoding'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2021-present BenitzCoding'
-__version__ = '1.0.8'
+__version__ = '1.0.9'
 
 from .compile import Compile
 # from .discord import Discord
 from .json_utils import *
+from .os_commands import *
