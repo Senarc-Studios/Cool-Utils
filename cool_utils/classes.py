d93f202a96242cf2d3270c971893d46e7cc5afc9
BenitzCoding <benitzcoding@yahoo.com>
2022-02-26 12:04:30 +0800

    Fixing module typo.

--- cool_utils/classes.py
+++ cool_utils/classes.py
@@ -30,7 +30,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-from Typing import Any, TypeVar
+from typing import Any, TypeVar
 
 __all__ = (
 	'Array'
