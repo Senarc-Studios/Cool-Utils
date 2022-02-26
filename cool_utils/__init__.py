a2c96d3995946a7305a9696855ae6a1841c07057
BenitzCoding <benitzcoding@yahoo.com>
2022-02-26 12:05:20 +0800

    Updated version number.

--- cool_utils/__init__.py
+++ cool_utils/__init__.py
@@ -34,7 +34,7 @@ __title__ = 'cool-utils'
 __author__ = 'BenitzCoding'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2021-present BenitzCoding'
-__version__ = '1.2.1.1'
+__version__ = '1.2.1.2'
 
 from .compile import Compile
 from .json_utils import JSON
