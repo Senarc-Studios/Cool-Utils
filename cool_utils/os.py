586396ad19f1049ed46122c1992897913f740ed7
BenitzCoding <benitzcoding@yahoo.com>
2022-04-01 15:39:12 +0800

    Removed `simple-chalk` import.

--- cool_utils/os.py
+++ cool_utils/os.py
@@ -31,7 +31,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 import os
 import sys
-from simple_chalk import chalk
 from datetime import datetime, tzinfo
 from typing import TypeVar
 
