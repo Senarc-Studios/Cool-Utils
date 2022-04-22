33558ae55e478a17046ac8edb7240b165b856c0b
BenitzCoding <benitzcoding@yahoo.com>
2022-04-22 20:13:05 +0800

    Updated Build Version in Setup file.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
 		"Issue tracker": "https://github.com/Senarc-Studios/Cool-Utils/issues",
 		"Github": "https://github.com/Senarc-Studios/Cool-Utils/tree/Development"
 	},
-	version="1.3.0",
+	version="1.3.1",
 	packages=packages,
 	license="BSD",
 	description="This is Cool Utility tools that you can use in python.",
