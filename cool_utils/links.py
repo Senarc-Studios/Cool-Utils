ee8e9d73abb065a59c48b252daf7fb0dfb13c2d8
BenitzCoding <benitzcoding@yahoo.com>
2022-05-08 22:38:40 +0800

    Added more features and made link detection more reliable.

--- cool_utils/links.py
+++ cool_utils/links.py
@@ -31,6 +31,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import re
+import string
+
 from typing import TypeVar
 
 from ._missing import MISSING
@@ -54,58 +56,106 @@ class Links:
 	@staticmethod
 	def check(content: str) -> bool:
 		word_list = []
-		character_list = [';', get_slash(), '|', '-', '"', '&', '$', '@', '+', '^', '_', '<', '[', '!', '=', '>', '(',
-		                  ')', '}', "'", '`', ']', '#', '%', '?', '*', '{', ',', '~']
+		ascii_chars = list(string.ascii_uppercase + string.ascii_lowercase + "0123456789.")
 
-		for character in character_list:
-			text = content.replace(character, " ")
-		for word in text.split(" "):
+		for character in content:
+			if character not in ascii_chars:
+				content = content.replace(character, " ")
+		for word in content.split(" "):
 			word_list.append(word)
+		_count = -1
 		for word in word_list:
+			_count += 1
+			if word == ".":
+				return True
 			if re.match(REGEX, word):
 				return True
 			else:
 				return False
 
 	@staticmethod
-	def censor(content: str, censor: str = "*") -> str:
+	def censor(content: str, censor: str = "*", strict: bool = True) -> str:
 		word_list = []
-		character_list = [';', get_slash(), '|', '-', '"', '&', '$', '@', '+', '^', '_', '<', '[', '!', '=', '>', '(',
-		                  ')', '}', "'", '`', ']', '#', '%', '?', '*', '{', ',', '~']
+		if strict:
+			_processed_words = []
+			ascii_chars = list(string.ascii_uppercase + string.ascii_lowercase + "0123456789.")
 
-		count = 0
-		string = ""
+			count = 0
+			_string = ""
 
-		for character in character_list:
-			text = content.replace(character, " ")
-		for word in text.split(" "):
-			word_list.append(word)
-		for word in word_list:
-			if re.match(REGEX, word):
-				count = count + 1
-				word = "".join([censor for i in range(len(word))])
+			for character in content:
+				if character not in ascii_chars:
+					content = content.replace(character, " ")
+			for word in content.split(" "):
+				word_list.append(word)
+			_count = -1
+			for word in word_list:
+				_count += 1
+				if word == ".":
+					try:
+						_processed_words[_count - 1] = "".join([censor for i in range(len(word_list[_count - 1]))])
+						_processed_words[_count] = censor
+						_processed_words[_count + 1] = "".join([censor for i in range(len(word_list[_count + 1]))])
+						count += 1
+						continue
+					except IndexError:
+						break
+				if re.match(REGEX, word):
+					count += 1
+					word = "".join([censor for i in range(len(word))])
+				_processed_words.append(word)
+				_string = _string + word + " "
 
-			string = string + word + " "
+			if count == 0:
+				return content
 
-		if count == 0:
-			return content
+			else:
+				final_string = ""
+				for string_ in _processed_words:
+					final_string += string_ + " "
+				return final_string
 
 		else:
-			return string
+			character_list = [';', get_slash(), '|', '-', '"', '&', '$', '@', '+', '^', '_', '<', '[', '!', '=', '>', '(',
+		                  ')', '}', "'", '`', ']', '#', '%', '?', '*', '{', ',', '~']
+
+			count = 0
+			_string = ""
+
+			for character in character_list:
+				text = content.replace(character, " ")
+			for word in text.split(" "):
+				word_list.append(word)
+			for word in word_list:
+				if re.match(REGEX, word):
+					count += 1
+					word = "".join([censor for i in range(len(word))])
+
+				_string = _string + word + " "
+
+			if count == 0:
+				return content
+
+			else:
+				return _string
 
 	@staticmethod
 	def find(content: str) -> list:
 		word_list = []
-		character_list = [';', get_slash(), '|', '-', '"', '&', '$', '@', '+', '^', '_', '<', '[', '!', '=', '>', '(',
-		                  ')', '}', "'", '`', ']', '#', '%', '?', '*', '{', ',', '~']
+		ascii_chars = list(string.ascii_uppercase + string.ascii_lowercase + "0123456789.")
 
 		link_list = []
-
-		for character in character_list:
-			text = content.replace(character, " ")
-		for word in text.split(" "):
-			word_list.append(word)
+		for character in content:
+			if character not in ascii_chars:
+				content = content.replace(character, " ")
+		for word in content.split(" "):
+			if word != "":
+				word_list.append(word)
+		count = -1
 		for word in word_list:
+			count += 1
+			if word == ".":
+				word = word_list[count - 1] + word + word_list[count + 1]
 			if re.match(REGEX, word):
 				link_list.append(word)
 			else:
