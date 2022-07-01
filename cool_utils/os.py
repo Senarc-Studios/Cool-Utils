974c92d75f5517c67a0ad23b5efff1b26a630ccd
BenitzCoding <benitzcoding@yahoo.com>
2022-07-01 22:25:28 +0800

    Removed unused import.

--- cool_utils/os.py
+++ cool_utils/os.py
@@ -31,7 +31,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 import os
 import sys
-from datetime import datetime, tzinfo
+from datetime import datetime
 from typing import TypeVar
 
 __all__ = (
